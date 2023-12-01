3e3049fa92bd4e9b40616b298768485b0bb52640
mdominguezd <martin.dominguez.duran@gmail.com>
2023-12-01 17:44:03 +0100

    Added folders where both interfaces will be built

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'HagueHaven', page_icon = ":house:")
+
+st.title(':house: HagueHaven')
+
+st.sidebar.radio('Gender (?)', ['Male', 'Female'])
+
+st.markdown("""
+bla bla description
+""")
+
+
+
