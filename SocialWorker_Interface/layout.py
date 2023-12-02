2c59d70222680c4ff550b8636244e5ace83209c1
mdominguezd <martin.dominguez.duran@gmail.com>
2023-12-03 00:24:39 +0100

    Social worker interface done

--- SocialWorker_Interface/layout.py
+++ SocialWorker_Interface/layout.py
@@ -8,32 +8,55 @@ def layout():
     
     st.sidebar.header('Quick filters:')
     gender = st.sidebar.radio('Gender:', ['Male', 'Female','Non-binary'])
-    age = st.sidebar.number_input('Age:', min_value = 0, step = 1, format = '%d')
+    age = st.sidebar.number_input('Age:', min_value = 0, value = 18, step = 1, format = '%d')
     family = st.sidebar.checkbox('Family housing')
     st.sidebar.markdown("#### Special care:")
-    drugs = st.sidebar.checkbox('Addiction assistance')
-    domestic_vio =  st.sidebar.checkbox('Domestic violence')
+    drugs = st.sidebar.checkbox('Substance abuse assistance')
     mental = st.sidebar.checkbox('Mental health assistance')
 
-    st.sidebar.button('Get results')
+    def click_results():
+        st.session_state.suit_clicked = False
+        st.session_state.results_clicked = True
+
+    results = st.sidebar.button('Get results', on_click = click_results)
     
-    tab1, tab2, tab3 = st.tabs(["Housing (:house:) Locations in Den Haag", "Fill in the form (:clipboard:)", 'Client information (:curly_haired_person:)'])
+    tab1, tab2, tab3 = st.tabs(["Housing (:house:) Locations in The Hague", "Fill in the form (:clipboard:)", 'Client information (:curly_haired_person:)'])
     
     
     with tab1:
         st.markdown("""
-        Housing solutions for The Hague
+        #### Housing locations at The Hague
         """)
 
-        c1, c2 = st.columns(2)
+        # BUTTON FUNCTIONS
+        def click_suit():
+            st.session_state.suit_clicked = True
+            st.session_state.results_clicked = False
+
+        c1, c2 = st.columns([5,1])
+
+        if 'suit_clicked' not in st.session_state:
+            st.session_state.suit_clicked = False
+            
+        if 'results_clicked' not in st.session_state:
+            st.session_state.results_clicked = False
 
         with c1:
-            id = st.number_input("ID")
+            id = st.number_input("ID", value = 10000, step = 1, format = '%d')
+            
         with c2:
-            button = st.button("Calculate suitabilty")
-
-        if button:
+            button = st.button("Calculate suitabilty", on_click = click_suit)
+                    
+        if st.session_state.suit_clicked:
             draw_map(id = id)
+        elif st.session_state.results_clicked:
+            filters = {'gender': gender,
+                       'age' : age,
+                       'family' : family,
+                       'drugs' : drugs,
+                       'mental' : mental}
+            
+            draw_map(filters = filters)
         else:
             draw_map()
         
--- SocialWorker_Interface/map_shelters.py
+++ SocialWorker_Interface/map_shelters.py
@@ -7,9 +7,10 @@ from owslib.wfs import WebFeatureService
 import os 
 from datetime import date
 import numpy as np
+import datetime 
 
 
-def draw_map(id = None, location = [52.07819576524207, 4.309696687447081]):
+def draw_map(id = None, filters = None, location = [52.07819576524207, 4.309696687447081]):
 
     if 'Data' not in os.listdir('.'):
         os.mkdir('Data')
@@ -32,38 +33,106 @@ def draw_map(id = None, location = [52.07819576524207, 4.309696687447081]):
             housings = housings.merge(suit_df, on = 'UniqueLocID', how = 'left')
         else:
             housings['Suitability'] = np.nan
+
+        if filters != None:
+            housings = filter_data(housings, filters)
             
         housings['RT_Occupation'] = housings['RT_Occupation'].fillna(0)
 
         housings['perc_ocup'] = 100*(housings['RT_Occupation']/housings['Number of places'])
 
         for i in range(len(housings)):
-            if housings.iloc[i]['Suitability'] == np.nan:
-                I = folium.Icon(color = '')
+            
+            if housings.iloc[i]['perc_ocup'] == 100:
+                I = folium.Icon(color = 'gray', icon = 'info-sign')
+                
             else:
-                if housings.iloc[i]['Suitability'] == 1:
-                    I = folium.Icon(color="green", icon="x")
-                
-                elif housings.iloc[i]['Suitability'] >= 0.7:
-                    I = folium.Icon(color="orange", icon="home")
-                
+                if np.isnan(housings.iloc[i]['Suitability']):
+                    
+                    if housings.iloc[i]['perc_ocup'] == 100:
+                        I = folium.Icon(color = 'gray', icon = 'home')
+                        
+                    else:
+                        I = folium.Icon(color = 'blue', icon = 'home')
+                        
                 else:
-                    I = folium.Icon(color = 'green', icon = 'home')
+                    if housings.iloc[i]['Suitability'] >= 0.75:
+                        I = folium.Icon(color="green", icon="home")
+                    
+                    elif housings.iloc[i]['Suitability'] >= 0.70:
+                        I = folium.Icon(color="orange", icon="home")
+                    
+                    else:
+                        I = folium.Icon(color = 'gray', icon = 'home')
                 
             folium.Marker(
                 location = [housings.iloc[i].geometry.y, housings.iloc[i].geometry.x],
                 icon= I,
-                tooltip = '<b>Occupation percent: </b>' + str(round(housings.iloc[i]['perc_ocup'], 0)) + '<br><b>Places available: </b>' + str((housings.iloc[i]['Number of places'] - housings.iloc[i]['RT_Occupation'])) + '<br><b>Suitability: </b>' + str(housings.iloc[i]['Suitability'])
+                popup = '<b>ID: </b>' + housings.iloc[i]['UniqueLocID'] + '<br><b>Adress: </b>' + housings.iloc[i]['Adres'] + '<br><b>Contact:</b>' + str(round(housings.iloc[i]['ContactPhone'])),
+                tooltip = '<b>Occupation percent: </b>' + str(round(housings.iloc[i]['perc_ocup'], 0)) + '<br><b>Places available: </b>' + str((housings.iloc[i]['Number of places'] - housings.iloc[i]['RT_Occupation'])) + '<br><b>Suitability: </b>' + str(round(housings.iloc[i]['Suitability']*100, 0))
                 ).add_to(m)
     
     map_info = st_folium(m, width = 800, 
                          height = 500)
 
-    st.markdown("Data from selected housing organization: ")
+    if map_info["last_object_clicked_popup"] is not None:
+        st.markdown("### Data from selected housing organization: ")
+        
+        c1, c2 = st.columns([7,1])
+        
+        with c1:
+            house_data = map_info["last_object_clicked_popup"].split('\n')
+            
+            st.write("**Address:** " + house_data[1].split(': ')[1])
+            st.write("**Contact:** " + house_data[2].split(':')[1])
+            
+            H_ID = map_info["last_object_clicked_popup"].split(' ')[1][:5]
+            C_ID = id
+            
+        with c2:
+            req = st.button("Request")
 
-    st.write(map_info)
+        if req:
+            request_housing(C_ID, H_ID)
+
+
+def filter_data(df, filters):
+
+    st.markdown("Housing locations filtered with **quick filters**:")
+    # Filter by gender
+    df = df[(df['Gender'].isna()) | (df['Gender'] == filters['gender'])]
+
+    # Filter by age
+    df = df[(filters['age'] > df['MinAge']) & ((filters['age'] < df['MaxAge']))]
+
+    # Filter by family housing
+    df = df[df['Family'] == filters['family']]
+
+    # Filter by substance abuse
+    if filters['drugs']:
+        df = df[df['SubstanceAbuse'] == filters['drugs']]
+        
+    # Filter by mental illness
+    df
     
+    return df
+    
+def request_housing(ID, HousID,
+                    h_fn = 'Data/House_Data.csv', 
+                    c_fn = 'Data/Client_Data.csv'):
 
+    df = pd.read_csv(c_fn)
+
+    idx = df[df['ID'] == ID].index
+
+    if df[df['ID'] == ID]['UniqueLocID'].isna().iloc[0]:
+        df.loc[idx,'Request'] = HousID
+        
+    else:
+        st.warning('The client has already been assigned to a housing location', icon="⚠️")
+
+    df.to_csv(c_fn, index = False)
+    
 
 def get_careorg_data(filename = 'Data/House_Data.csv'):
     
@@ -92,74 +161,80 @@ def calculate_suitability(client_id, client_folder = 'Data/Client_Data.csv', hou
     house_df = pd.read_csv(house_folder, sep=",")
 
     client_df = pd.read_csv(client_folder, sep=",")
-
+    
     suit_df = pd.DataFrame(house_df["UniqueLocID"])
+    
+    if (client_id in list(client_df['ID'])):
+    
+    
+        today = datetime.date.today()
+        age = today.year - client_df.loc[client_df.ID == client_id, "YearofBirth"].values[0]
+        gender = client_df.loc[client_df.ID == client_id, "Gender"].values[0]
+        mental = client_df.loc[client_df.ID == client_id, "MentalHealth"].values[0]
+        service = client_df.loc[client_df.ID == client_id, "Service"].values[0]
+        addiction = client_df.loc[client_df.ID == client_id, "SubstanceAbuse"].values[0]
+        prison = client_df.loc[client_df.ID == client_id, "Prison"].values[0]
+        family = client_df.loc[client_df.ID == client_id, "Family"].values[0]
+    
+        def geo_mean(iterable):
+            a = np.array(iterable)
+            return a.prod()**(1.0/len(a))
+    
+    
+        suit_list = []
+    
+        for index, row in house_df.iterrows():
+    
+            suitabilities = []
+    
+            if row["MinAge"] < age < row["MaxAge"]:
+                suitabilities.append(1.)
+            else:
+                suitabilities.append(0.)
+    
+            if pd.isna(row["Gender"]):
+                suitabilities.append(0.5)
+            elif row["Gender"] == gender:
+                suitabilities.append(1.)
+            else:
+                suitabilities.append(0.)
+    
+            if row["MentalHealth"]==mental:
+                suitabilities.append(1.)
+            else:
+                suitabilities.append(0.)
+    
+            if row["Service"]==service:
+                suitabilities.append(1.)
+            else:
+                suitabilities.append(0.)
+    
+            if pd.isna(row["SubstanceAbuse"]):
+                suitabilities.append(0.5)
+            elif row["SubstanceAbuse"]==addiction:
+                suitabilities.append(1.)
+    
+            if pd.isna(row["Prison"]):
+                suitabilities.append(0.5)
+            elif row["Prison"]==prison:
+                suitabilities.append(1.)
+    
+            if row["Family"]==family:
+                suitabilities.append(1.)
+            else:
+                suitabilities.append(0.)
+    
+            suit = geo_mean(suitabilities)
+    
+            suit_list.append(suit)
+    
+        suit_df["Suitability"] = suit_list
 
-
-    today = datetime.date.today()
-    age = today.year - client_df.loc[client_df.ID == client_id, "YearofBirth"].values[0]
-    gender = client_df.loc[client_df.ID == client_id, "Gender"].values[0]
-    mental = client_df.loc[client_df.ID == client_id, "MentalHealth"].values[0]
-    service = client_df.loc[client_df.ID == client_id, "Service"].values[0]
-    addiction = client_df.loc[client_df.ID == client_id, "SubstanceAbuse"].values[0]
-    prison = client_df.loc[client_df.ID == client_id, "Prison"].values[0]
-    family = client_df.loc[client_df.ID == client_id, "Family"].values[0]
-
-    def geo_mean(iterable):
-        a = np.array(iterable)
-        return a.prod()**(1.0/len(a))
-
-
-    suit_list = []
-
-    for index, row in house_df.iterrows():
-
-        suitabilities = []
-
-        if row["MinAge"] < age < row["MaxAge"]:
-            suitabilities.append(1.)
-        else:
-            suitabilities.append(0.)
-
-        if pd.isna(row["Gender"]):
-            suitabilities.append(0.5)
-        elif row["Gender"] == gender:
-            suitabilities.append(1.)
-        else:
-            suitabilities.append(0.)
-
-        if row["MentalHealth"]==mental:
-            suitabilities.append(1.)
-        else:
-            suitabilities.append(0.)
-
-        if row["Service"]==service:
-            suitabilities.append(1.)
-        else:
-            suitabilities.append(0.)
-
-        if pd.isna(row["SubstanceAbuse"]):
-            suitabilities.append(0.5)
-        elif row["SubstanceAbuse"]==addiction:
-            suitabilities.append(1.)
-
-        if pd.isna(row["Prison"]):
-            suitabilities.append(0.5)
-        elif row["Prison"]==prison:
-            suitabilities.append(1.)
-
-        if row["Family"]==family:
-            suitabilities.append(1.)
-        else:
-            suitabilities.append(0.)
-
-        suit = geo_mean(suitabilities)
-
-        suit_list.append(suit)
-
-    suit_df["Suitability"] = suit_list
+    else:
+        suit_df["Suitability"] = np.nan
 
     return suit_df
+    
     
 # def calculate_suitability(ID = 11, c_fn = 'Data/Client_Data.csv', h_fn = 'Data/House_Data.csv'):
 
